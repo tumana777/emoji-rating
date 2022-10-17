37e9bd579693238f618cecda890030f92e2f683b
tumana777 <tumana991@gmail.com>
2022-10-17 17:43:55 +0400

    Create Emoji Rating

---
+++ app.js
@@ -0,0 +1,26 @@
+const starsEl = document.querySelectorAll('.fa-star');
+const emojisEl = document.querySelectorAll('.fa-regular');
+const colors = ['red', 'orange', 'lightblue', 'lightgreen', 'green'];
+
+starsEl.forEach((starEl, index) => {
+    starEl.addEventListener('click', () => {
+        updateRating(index)
+    });
+});
+
+updateRating(0);
+
+function updateRating(index) {
+    starsEl.forEach((starEl, ind) => {
+        if (ind < index + 1) {
+            starEl.classList.add('active');
+        } else {
+            starEl.classList.remove('active');
+        }
+    });
+
+    emojisEl.forEach((emojiEl) => {
+        emojiEl.style.transform = `translateX(-${index * 50}px)`;
+        emojiEl.style.color = colors[index];
+    })
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+const starsEl = document.querySelectorAll('fa-star');
